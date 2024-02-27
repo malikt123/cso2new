a2132404bbda3e1a912ac6bcbba08887eea2d774
Malik Thomas <bjn5zk@virginia.edu>
2024-02-27 15:52:15 -0500

    removed an error

--- pagetable_1/pagetable.c
+++ pagetable_1/pagetable.c
@@ -14,11 +14,17 @@ typedef struct {
     size_t physical_page_number;
 } PageTableEntry;
 
+size_t ptbr;
+
 // Define helper functions if needed
+void setup_page_table() {
+    // Initialize ptbr with the base address of the page table
+    ptbr = 0; 
+}
 
 size_t translate(size_t va) {
     size_t offset_mask = (1 << POBITS) - 1;
-    size_t pt_index_mask = (PAGE_TABLE_SIZE - 1) << POBITS;
+    size_t pt_index_mask = ((size_t)PAGE_TABLE_SIZE - 1) << POBITS;
     size_t pt_index_shift = 64 - POBITS;
 
     // get page table indices from v address
@@ -36,11 +42,24 @@ size_t translate(size_t va) {
         }
         pt_address = pte.physical_page_number & ~(PTE_VALID_BIT);
         
-        pt_index = (va >> (pt_index_shift - (level + 1) * POBITS)) & (PAGE_TABLE_SIZE - 1); // update page table
+        pt_index = (va >> ((size_t)(pt_index_shift - (level + 1) * POBITS))) & (PAGE_TABLE_SIZE - 1); // update page table
     }
 
     // determine physical address
     size_t physical_address = (pt_address << POBITS) | offset;
 
     return physical_address;
+}
+
+int main() {
+    // Your main program logic here
+    setup_page_table();
+
+    // Example usage of translate function
+    size_t virtual_address = 0x123456789ABCDEF;
+    size_t physical_address = translate(virtual_address);
+
+    printf("Physical Address: %lu\n", physical_address);
+
+    return 0;
 }
